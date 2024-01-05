b7b6a498024572e283c85e81ae9c972dfbf1e508
danyealmateen <danyeal.mateen@hotmail.com>
2024-01-05 20:48:49 +0100

    more routes added

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -14,9 +14,24 @@ app.get('/api/products/', (req, res) => {
   res.json(newProducts);
 });
 
-app.get('/api/products/1', (req, res) => {
-  const singleProduct = products.find((product) => product.id === 1);
-  res.json(singleProduct);
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req);
+  //console.log(req.params)
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+
+  if (!singleProduct) {
+    return res.status(404).send('Product Does Not Exist');
+  }
+  return res.json(singleProduct);
+});
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+  console.log(req.params);
+  res.send('hello world');
 });
 
 app.listen(5000, (req, res) => {
